c857e7759825f3d2061e3c20b90fb712f37a2aec
lihe07 <li@imlihe.com>
2023-07-07 10:12:24 +0800

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,126 +14,134 @@ import {
   Icon,
   Input,
 } from "@chakra-ui/react";
-import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-import {checkUsername} from "../api";
+import { checkUsername } from "../api";
 
-import {BsMicrosoft} from "react-icons/bs";
+import { BsMicrosoft } from "react-icons/bs";
 
 export default function Login() {
-    const [isLoading, setIsloading] = useState(false);
-    const [error, setError] = useState("");
-    const [username, setUsername] = useState("");
-    const router = useRouter();
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const router = useRouter();
 
-    function goMicrosoft() {
-        const current = window.location.href;
-        location.href = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=a9924c31-f355-4fe9-a7e5-bd72e140d820&response_type=code&redirect_uri=" +
-            encodeURIComponent(current) +
-            "&scope=XboxLive.signin%20offline_access%20openid%20email&prompt=select_account&response_mode=fragment";
+  function goMicrosoft() {
+    const current = window.location.href;
+    location.href =
+      "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=a9924c31-f355-4fe9-a7e5-bd72e140d820&response_type=code&redirect_uri=" +
+      encodeURIComponent(current) +
+      "&scope=XboxLive.signin%20offline_access%20openid%20email&prompt=select_account&response_mode=fragment";
+  }
+
+  async function login(_code: string | undefined) {
+    if (isLoading) return;
+
+    let code;
+    if (_code) {
+      code = _code;
+    } else {
+      code = username;
     }
 
-    async function login() {
-        if (isLoading) return;
+    setIsloading(true);
+    if (code.length < 3) {
+      setError("Auth Code 长度不足");
 
-        setIsloading(true);
-        if (username.length < 3) {
-            setError("Auth Code 长度不足");
+      setTimeout(() => {
+        setIsloading(false);
+      }, 1000);
 
-            setTimeout(() => {
-                setIsloading(false);
-            }, 1000);
-
-            return;
-        }
-        if (!(await checkUsername(username))) {
-            setError("Auth Code 无效");
-            setIsloading(false);
-            return;
-        }
-
-        localStorage.setItem("username", username);
-        router.push("/dash");
+      return;
+    }
+    if (!(await checkUsername(code))) {
+      setError("Auth Code 无效");
+      setIsloading(false);
+      return;
     }
 
-    useEffect(() => {
-        let rawHash = window.location.hash;
-        if (rawHash.length < 2) return;
+    localStorage.setItem("username", code);
+    router.push("/dash");
+  }
 
-        rawHash = rawHash.substring(1);
-        const hash = new URLSearchParams(rawHash);
+  useEffect(() => {
+    let rawHash = window.location.hash;
+    if (rawHash.length < 2) return;
 
-        if (hash.has("error")) {
-            setError("登录失败: " + hash.get("error_description"));
-            return;
-        }
-        if (hash.has("code")) {
-            const code = hash.get("code")!;
-            setUsername(code);
-            login();
-            return;
-        }
-    }, []);
+    rawHash = rawHash.substring(1);
+    const hash = new URLSearchParams(rawHash);
 
-    return (
-        <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            minH="100vh"
-        >
-            <Card>
-                <CardBody p={"10"}>
-                    <Heading textAlign={"center"} fontSize="30">
-                        登录
-                    </Heading>
+    if (hash.has("error")) {
+      setError("登录失败: " + hash.get("error_description"));
+      return;
+    }
+    if (hash.has("code")) {
+      const code = hash.get("code")!;
+      setUsername(code);
+      login(code);
+      return;
+    }
+  }, []);
 
-                    <Button
-                        onClick={goMicrosoft}
-                        colorScheme="blue"
-                        variant="outline"
-                        m="auto"
-                        mt="5"
-                        display="block"
-                        w="full"
-                    >
-                        <Icon as={BsMicrosoft} mr="2"></Icon>
-                        使用微软登录
-                    </Button>
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minH="100vh"
+    >
+      <Card>
+        <CardBody p={"10"}>
+          <Heading textAlign={"center"} fontSize="30">
+            登录
+          </Heading>
 
-                    <Divider my="5"/>
+          <Button
+            onClick={goMicrosoft}
+            colorScheme="blue"
+            variant="outline"
+            m="auto"
+            mt="5"
+            display="block"
+            w="full"
+          >
+            <Icon as={BsMicrosoft} mr="2"></Icon>
+            使用微软登录
+          </Button>
 
-                    <Input
-                        mb="5"
-                        placeholder="Auth Code"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter") {
-                                login();
-                            }
-                        }}
-                    />
+          <Divider my="5" />
 
-                    <Button
-                        pt="1.5"
-                        w="full"
-                        colorScheme="blue"
-                        isLoading={isLoading}
-                        onClick={login}
-                    >
-                        继续
-                    </Button>
+          <Input
+            mb="5"
+            placeholder="Auth Code"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                login();
+              }
+            }}
+          />
 
-                    <Collapse in={!!error} animateOpacity>
-                        <Alert status="error" mt="5">
-                            <AlertIcon/>
-                            <AlertDescription>{error}</AlertDescription>
-                        </Alert>
-                    </Collapse>
-                </CardBody>
-            </Card>
-        </Box>
-    );
+          <Button
+            pt="1.5"
+            w="full"
+            colorScheme="blue"
+            isLoading={isLoading}
+            onClick={login}
+          >
+            继续
+          </Button>
+
+          <Collapse in={!!error} animateOpacity>
+            <Alert status="error" mt="5">
+              <AlertIcon />
+              <AlertDescription>{error}</AlertDescription>
+            </Alert>
+          </Collapse>
+        </CardBody>
+      </Card>
+    </Box>
+  );
 }
