96e7ed823c1b1c923885cc677c50093ab82b68cf
damesck233 <damesck@qq.com>
2025-06-15 14:29:22 +0800

    Update StagingArea.tsx
    Co-Authored-By: Snowball_233 <wangrb.qq@outlook.com>

--- components/upload/StagingArea.tsx
+++ components/upload/StagingArea.tsx
@@ -87,7 +87,7 @@ export default function StagingArea({ uploadOptions }: StagingAreaProps) {
 
       await uploadFiles(files, {
         ...uploadOptions,
-        metadata: { customNames }
+        metadata: { customNames: JSON.stringify(customNames) }
       });
     }
 
